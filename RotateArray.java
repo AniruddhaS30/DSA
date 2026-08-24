446ef11520ea8b717a89d06ce494dc82150473d5
Aniruddha S <aniruddhashankar.blr@gmail.com>
2026-08-24 22:49:14 +0530

    Updated RotateArray code

--- RotateArray.java
+++ RotateArray.java
@@ -7,7 +7,7 @@ public class RotateArray {
         reverse(nums, k, n-1);
 
     }
-    public static void reverse(int [] nums, int start, int end){
+    public static void reverse(int [] nums, int start, int end){//two pointer approach
         while(start<end){
             int temp = nums[start];
             nums[start] = nums[end];
@@ -27,3 +27,6 @@ public class RotateArray {
         }
     }
 }
+/* In this specific code to rotate the array by k places(right rotate) in the first rotate func we rotate the nums
+, firstly we reverse the entire array, then reverse the first k elements, and finally reverse the remaining elements 
+the reverse() function is using the two-pointer approach to reverse a specific portion of the array in-place.*/
