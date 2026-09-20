0c7e9e74b7b8527be5bdd3b6ddf96ab9572d3097
Aniruddha S <aniruddhashankar.blr@gmail.com>
2026-09-20 17:53:04 +0530

    Maximum Consecutive Ones

--- MaxConsecutiveOnes.java
+++ MaxConsecutiveOnes.java
@@ -1,3 +1,11 @@
+/* 
+Example 1:
+Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+Example 2:
+Input: nums = [1,0,1,1,0,1]
+Output: 2 */
 public class MaxConsecutiveOnes {
     public static int findMaxConsecutiveOnes(int nums[]){
         int count = 0, maxCount = 0;
